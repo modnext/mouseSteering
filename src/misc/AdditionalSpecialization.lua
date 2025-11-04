6302e2bc740c44993c265dba4bb08592840c1072
Sławek Jaskulski <Blejder4k@gmail.com>
2025-11-04 17:36:25 +0100

    fix: exclude locomotives

--- src/misc/AdditionalSpecialization.lua
+++ src/misc/AdditionalSpecialization.lua
@@ -20,12 +20,13 @@ function AdditionalSpecialization.finalizeTypes(self)
   -- compose the full specialization name using the mod's name
   local specialization = modName .. ".mouseSteeringVehicle"
 
-  -- add mouse steering specialization to drivable vehicles
+  -- add mouse steering specialization to drivable vehicles (except locomotives)
   for typeName, typeEntry in pairs(self:getTypes()) do
     local hasDrivable = SpecializationUtil.hasSpecialization(Drivable, typeEntry.specializations)
+    local hasLocomotive = SpecializationUtil.hasSpecialization(Locomotive, typeEntry.specializations)
     local hasMouseSteering = SpecializationUtil.hasSpecialization(specialization, typeEntry.specializations)
 
-    if hasDrivable and not hasMouseSteering then
+    if hasDrivable and not hasLocomotive and not hasMouseSteering then
       self:addSpecialization(typeName, specialization)
     end
   end
