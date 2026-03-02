c8c95e46823b0225f13f5c0ab3c03eaf0b663fc1
Sławek Jaskulski <Blejder4k@gmail.com>
2026-03-02 18:25:50 +0100

    refactor: improve AdditionalSpecialization readability

--- src/misc/AdditionalSpecialization.lua
+++ src/misc/AdditionalSpecialization.lua
@@ -10,14 +10,14 @@ local modName = g_currentModName
 
 AdditionalSpecialization = {}
 
----Finalizes vehicle types
--- @param self table self
+---Finalize vehicle types
+-- @param table self the type manager
 function AdditionalSpecialization.finalizeTypes(self)
   if self.typeName ~= "vehicle" then
     return
   end
 
-  -- compose the full specialization name using the mod's name
+  -- compose full specialization names
   local mouseSteeringSpecialization = modName .. ".mouseSteeringVehicle"
   local mouseSteeringSpeedControlSpecialization = modName .. ".mouseSteeringSpeedControl"
 
@@ -26,17 +26,15 @@ function AdditionalSpecialization.finalizeTypes(self)
     local hasDrivable = SpecializationUtil.hasSpecialization(Drivable, typeEntry.specializations)
     local hasLocomotive = SpecializationUtil.hasSpecialization(Locomotive, typeEntry.specializations)
 
-    -- attach mouse steering to drivable vehicles (except locomotives)
+    -- attach specializations to drivable vehicles (except locomotives)
     if hasDrivable and not hasLocomotive then
       local hasMouseSteering = SpecializationUtil.hasSpecialization(mouseSteeringSpecialization, typeEntry.specializations)
+      local hasMouseSteeringSpeedControl = SpecializationUtil.hasSpecialization(mouseSteeringSpeedControlSpecialization, typeEntry.specializations)
+
       if not hasMouseSteering then
         self:addSpecialization(typeName, mouseSteeringSpecialization)
       end
-    end
 
-    -- attach speed control to drivable vehicles (except locomotives)
-    if hasDrivable and not hasLocomotive then
-      local hasMouseSteeringSpeedControl = SpecializationUtil.hasSpecialization(mouseSteeringSpeedControlSpecialization, typeEntry.specializations)
       if not hasMouseSteeringSpeedControl then
         self:addSpecialization(typeName, mouseSteeringSpeedControlSpecialization)
       end
